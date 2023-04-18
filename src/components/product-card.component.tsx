5c3ac2acf61ab535ab7e14a5c7f9e31bed9194ee
Kleanton1 <Kleanton1>
2023-04-18 11:56:58 +0100

    changes

--- src/components/product-card.component.tsx
+++ src/components/product-card.component.tsx
@@ -18,7 +18,7 @@ const ProductCard = () => {
          
         <div>
         <img 
-        src="https://images.pexels.com/photos/106839/pexels-photo-106839.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        src="https://img.freepik.com/free-vector/wicker-hanging-chairs-swing-hanging-chain-with-cushions_1284-45541.jpg?size=626&ext=jpg&ga=GA1.2.819716268.1681813828&semt=ais"
         className="card-img-chair" 
         alt="img-chair" 
         height="200"
@@ -30,6 +30,36 @@ const ProductCard = () => {
             count={5}
             size={20}
             color2={'#ffd700'} />
+
+            <div>
+              <img 
+              src="https://img.freepik.com/premium-psd/headphone-mockup_187299-1061.jpg?w=740" 
+              alt="img-headphones"
+              className="card-img-headphone" 
+              height= "200" 
+              width= "200"             
+              />
+              <h5 className="card-title">PSD headphone
+          </h5>
+          <ReactStars
+            count={5}
+            size={20}
+            color2={'#ffd700'} />
+            <div>
+              <img 
+              src="https://img.freepik.com/free-photo/laptop-with-blank-black-screen-white-table_53876-97915.jpg?w=740&t=st=1681815167~exp=1681815767~hmac=363194417a64d5d032d566841a6485d139e0b9927bc749067d8e4967f7b23b03" 
+              alt="img-laptop"
+              className="card-img-laptop"
+              height= "200"
+              width="200"
+              />
+              <h5 className="card-title">Dell laptop
+          </h5>
+          <ReactStars
+            count={5}
+            size={20}
+            color2={'#ffd700'} />
+            </div>
          </div>
         </div>
         </div>
@@ -37,6 +67,7 @@ const ProductCard = () => {
         <a href="#" className="btn btn-primary">
           Go somewhere
         </a>
+      </div>
       </div>
   );
 };
