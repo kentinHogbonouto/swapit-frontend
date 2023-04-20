a25f071dceb9c525f8aaaa1dce3561a3a4458b77
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2023-04-20 20:39:37 +0100

    fix: update banner

--- src/components/banner.component.tsx
+++ src/components/banner.component.tsx
@@ -5,63 +5,23 @@ import promotionImage from "../public/images/promotion_bg.png";
 const Banner = () => {
   return (
     <div className="container-fluid banner-container">
-      <div className="row">
-        <div className="col-lg-12">
-          <div id="carouselExample" className="carousel slide">
-            <div className="carousel-inner">
-              <div className="carousel-item active">
-                <Image
-                  src="/images/promotion_bg.png"
-                  className="d-block w-100"
-                  alt="..."
-                  width={500}
-                  height={500}
-                />
-              </div>
-              <div className="carousel-item">
-                <Image
-                  src="/images/promotion_bg.png"
-                  className="d-block w-100"
-                  alt="..."
-                  width={500}
-                  height={500}
-                />
-              </div>
-              <div className="carousel-item">
-                <Image
-                  src="/images/promotion_bg.png"
-                  className="d-block w-100"
-                  alt="..."
-                  width={500}
-                  height={500}
-                />
-              </div>
-            </div>
-            <button
-              className="carousel-control-prev"
-              type="button"
-              data-bs-target="#carouselExample"
-              data-bs-slide="prev"
-            >
-              <span
-                className="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button
-              className="carousel-control-next"
-              type="button"
-              data-bs-target="#carouselExample"
-              data-bs-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Next</span>
-            </button>
-          </div>
+      <div className="row banner-container">
+        <div className="col-lg-6 d-flex flex-column align-items-start justify-content-center">
+          <small>Best Furniture For Your Castle....</small>
+          <h1>New Furniture Collection Trends in 2020</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in
+            est adipiscing in phasellus non in justo.
+          </p>
+          <button type="submit" className="btn btn-danger">Shop Now</button>
+        </div>
+        <div className="col-lg-6 d-flex justify-content-center align-items-center">
+          <img
+            src="/images/sofa_promotional_header.png"
+            alt=""
+            width="100%"
+            height="auto"
+          />
         </div>
       </div>
     </div>
