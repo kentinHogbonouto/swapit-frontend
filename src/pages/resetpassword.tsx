9b26f7f7d268214bd7031b79cb0514e33c4dab02
kentinHogbonouto <kentinhogbonouto1@gmail.com>
2023-04-20 07:21:42 +0100

    fix: review reset password page

--- src/pages/resetpassword.tsx
+++ src/pages/resetpassword.tsx
@@ -1,34 +1,50 @@
-import React from 'react'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css"; 
 
 function resetpassword() {
   return (
-    <div className='main'>
-       <section className="sign-in">
-            <div className="container">
-                <div className="signin-content">
-                    <div className="signin-image">
-                        <figure><img src="https://cdni.iconscout.com/illustration/premium/thumb/login-page-4468581-3783954.png" alt="login image" /></figure>
-                        <a href="/register" className="signup-image-link">Create an account</a>
-                    </div>
-
-                    <div className="signin-form">
-                        <h2 className="form-title">Reset your password</h2>
-                        <form method="POST" className="register-form" id="login-form">
-                            <div className="form-group">
-                                
-                                <input type="email" name="email" id="your_name" placeholder="Your Email"/>
-                            </div>
-                            <div className="form-group form-button">
-                                <input type="submit" name="signin" id="signin" className="form-submit" value="Reset"/>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+    <div className="main">
+      <section className="sign-in">
+        <div className="container">
+          <div className="signin-content">
+            <div className="signin-image">
+              <figure>
+                <img
+                  src="https://cdni.iconscout.com/illustration/premium/thumb/login-page-4468581-3783954.png"
+                  alt="login image"
+                />
+              </figure>
+              <a href="/register" className="signup-image-link">
+                Create an account
+              </a>
             </div>
-        </section>
 
+            <div className="signin-form d-flex flex-column align-items-center justify-content-center">
+              <h2 className="form-title">Reset your password</h2>
+              <form method="POST" className="register-form" id="login-form">
+                <div className="form-group">
+                  <input
+                    type="email"
+                    name="email"
+                    id="your_name"
+                    placeholder="Your Email"
+                  />
+                </div>
+                <div className="form-group form-button">
+                  <button
+                    type="submit"
+                    name="signin"
+                    id="signin"
+                    className="btn btn-primary w-100"
+                  >Reset</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default resetpassword
+export default resetpassword;
